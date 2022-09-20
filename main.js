8511b7759f7790d06ac34d839c9b2a882c3752fa
Chanwoo <tree698@gmail.com>
2022-09-20 19:08:36 +0900

    JS: finter images + navbar button state

--- main.js
+++ main.js
@@ -66,3 +66,79 @@ const navBarMenuBar = document.querySelector('.navBar__menu-bar');
 navBarMenuBar.addEventListener('click', () => {
   navBarMenu.classList.toggle('hide');
 });
+
+// Filter images
+const galleryCategories = document.querySelector('.gallery__categories');
+const galleryContainer = document.querySelector('.gallery__container');
+const gallery = document.querySelectorAll('.gallery');
+
+galleryCategories.addEventListener('click', (e) => {
+  const filter = e.target.dataset.name || e.target.parentNode.dataset.name;
+  if (filter == null) {
+    return;
+  }
+
+  addActive(galleryCategories, filter);
+  galleryContainer.classList.add('anim-out');
+
+  setTimeout(() => {
+    gallery.forEach((g) => {
+      if (filter === 'all' || g.dataset.name === filter) {
+        g.classList.remove('hide');
+      } else {
+        g.classList.add('hide');
+      }
+    });
+    galleryContainer.classList.remove('anim-out');
+  }, 300);
+});
+
+// navbar button state
+navBarMenu.addEventListener('click', (e) => {
+  const target = e.target.dataset.link;
+  addActive(navBarMenu, target);
+});
+
+function addActive(element, dataName) {
+  for (const child of element.children) {
+    (child.dataset.name || child.dataset.link) === dataName
+      ? child.classList.add('active')
+      : child.classList.remove('active');
+  }
+}
+
+// const allBtn = document.querySelector('.category__btn.all');
+// const babyBtn = document.querySelector('.category__btn.baby');
+// const growBtn = document.querySelector('.category__btn.grow');
+// const adultBtn = document.querySelector('.category__btn.adult');
+
+// allBtn.addEventListener('click', () => {
+//   displayAll(galleryContainer);
+//   addActive(galleryCategories, 'all');
+// });
+// babyBtn.addEventListener('click', () => {
+//   filterImages(galleryContainer, 'baby');
+//   addActive(galleryCategories, 'baby');
+// });
+// growBtn.addEventListener('click', () => {
+//   filterImages(galleryContainer, 'grow');
+//   addActive(galleryCategories, 'grow');
+// });
+// adultBtn.addEventListener('click', () => {
+//   filterImages(galleryContainer, 'adult');
+//   addActive(galleryCategories, 'adult');
+// });
+
+// function filterImages(element, dataName) {
+//   for (const child of element.children) {
+//     child.dataset.name !== dataName
+//       ? child.classList.add('hide')
+//       : child.classList.remove('hide');
+//   }
+// }
+
+// function displayAll(element) {
+//   for (const child of element.children) {
+//     child.classList.remove('hide');
+//   }
+// }
